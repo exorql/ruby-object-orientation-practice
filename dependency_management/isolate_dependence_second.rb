f4cdd18ac3652a7130983683ea58098145e16cc3
haruto.watanabe <w.haru10n@gmail.com>
2018-10-04 01:08:26 +0900

    isolate_dependence_second end

--- dependency_management/isolate_dependence_second.rb
+++ dependency_management/isolate_dependence_second.rb
@@ -11,4 +11,29 @@ end
 Gear.new(
   :chainring => 52,
   :cog       => 11,
-  :wheel     => Wheel.new(26, 1.5).gear_inches  
+  :wheel     => Wheel.new(26, 1.5).gear_inches
+
+# Gearが外部インターフェースの一部分
+module SomeFramework
+  class Gear
+    attr_reader :chainring, :cog, :wheel
+      def initialize(args)
+      @chainring = args[:chainring]
+      @cog = args[:cog]
+      @wheel = args[:wheel]
+    end
+  end
+end
+
+# 外部のインターフェースをラップし、自身の変更から守る
+module GearWrapper
+  def self.gear(args)
+    SomeFramework::Gear.new(args[:chainring], args[:cog], args[:wheel])
+  end
+end
+
+# 引数を持つハッシュを渡すことでGearのインスタンスを作成できるようになった
+GearWrapper.gear(
+  :chainring => 52,
+  :cog       => 11,
+  :wheel     => Wheel.new(26, 1.5).gear_inches

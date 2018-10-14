a9c1535e7cf2285b941868b87d949b93fab90c58
haruto.watanabe <w.haru10n@gmail.com>
2018-10-14 20:57:49 +0900

    Ducktyping

---
+++ duck_typing/duck_typing.rb
@@ -0,0 +1,36 @@
+class Trip
+  attr_reader :bicycles, :customers, :vehicle
+
+  def prepare(preparers)
+    preparers.each { |preparer|
+      preparer.prepare_trip(self)}
+  end
+end
+
+# 全てのPreparerはprepare_tripに応答するダック
+class Mechanic
+  def prepare_trip(trip)
+    trip.bicycles.each { |bicyle|
+      prepare_bicycle(bicycle)}
+  end
+
+  # ...
+end
+
+class TripCoordinator
+  def prepare_trip(trip)
+    buy_food(trip.customers)
+  end
+
+  # ...
+end
+
+class Driver
+  def prepare_trip(trip)
+    vehicle = trip.vehicle
+    gas_up(vehicle)
+    fill_water_tank(vehicle)
+  end
+
+  # ...
+end

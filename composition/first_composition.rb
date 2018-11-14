fad23330fd169a5efaf38381e4fe30f81cb47fe5
haruto.watanabe <w.haru10n@gmail.com>
2018-11-14 10:06:38 +0900

    typo

--- composition/first_composition.rb
+++ composition/first_composition.rb
@@ -29,6 +29,6 @@ class Part
   def initialize(args)
     @name = args[:name]
     @description = args[:description]
-    @needs_spare = args[:needs_spare]
+    @needs_spare = args.fetch(:needs_spare, true)
   end
 end

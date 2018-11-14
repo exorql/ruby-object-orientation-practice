b242d401ef883683021dd83e7dd16676e24989cb
haruto.watanabe <w.haru10n@gmail.com>
2018-11-14 10:04:15 +0900

    composition/first_composition.rb

---
+++ composition/first_composition.rb
@@ -0,0 +1,34 @@
+class Bicycle
+  attr_reader :size, :parts
+
+  def initialize(args = {})
+    @size = args[:size]
+    @parts = args[:parts]
+  end
+
+  def spares
+    parts.spares
+  end
+end
+
+class Parts
+  attr_reader :parts
+
+  def initialize(parts)
+    @parts = parts
+  end
+
+  def spares
+    parts.select(&:needs_spare)
+  end
+end
+
+class Part
+  attr_reader :name, description, needs_spare
+
+  def initialize(args)
+    @name = args[:name]
+    @description = args[:description]
+    @needs_spare = args[:needs_spare]
+  end
+end
